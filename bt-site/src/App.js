9f8b50eac9b1bb55920285ff2bad654587e6be4c
Brenden Thornton <thorntonbrenden@gmail.com>
2019-04-24 17:18:34 -0600

    added Header and Footer components

--- bt-site/src/App.js
+++ bt-site/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 function App() {
   return (
     <div>
+      <Header />
       <h1>Hi, I'm Brenden Thornton</h1>
+      <Footer />
     </div>
   );
 }
