dac7743bab2c70d2934d4e10a12db21b31de7082
Brenden Thornton <thorntonbrenden@gmail.com>
2019-04-24 19:23:55 -0600

    created Landing component

---
+++ bt-site/src/components/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Landing = () => {
+
+  return(
+    <div>
+      <h1>Brenden Thornton</h1>
+      <h2>Full Stack Engineer</h2>
+    </div>
+  )
+}
+
+export default Landing
