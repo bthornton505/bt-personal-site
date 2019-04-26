878eac7d3b9cc5fca4bd5e9c36d1e8a2079abf2e
Brenden Thornton <thorntonbrenden@gmail.com>
2019-04-26 16:52:26 -0600

    created a projects component

---
+++ bt-site/src/components/Projects.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Projects = () => {
+
+  return(
+    <section id="projects">
+      <div>
+        <h2>Projects</h2>
+      </div>
+    </section>
+  )
+}
+
+export default Projects
