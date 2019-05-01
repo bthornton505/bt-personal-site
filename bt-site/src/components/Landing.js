9a1c2a88ea90d2517f57c7d3a07c5a894e85d7e6
Brenden Thornton <thorntonbrenden@gmail.com>
2019-05-01 13:41:46 -0600

    applied my new css classes but still needs work

--- bt-site/src/components/Landing.js
+++ bt-site/src/components/Landing.js
@@ -7,11 +7,11 @@ const Landing = () => {
       <div id="landing-picture">
         <img src="https://www.outtherecolorado.com/wp-content/uploads/2017/07/Denver-OutThere-Colorado.jpg" alt="denver"></img>
       </div>
-      <div>
-        <h1>Brenden Thornton</h1>
+      <div id="main-header">
+        <h1 className="main-title">Brenden Thornton</h1>
       </div>
-      <div>
-        <h2>Full Stack Engineer</h2>
+      <div id="job-header">
+        <h2 className="job-title">Full Stack Engineer</h2>
       </div>
     </section>
   )
