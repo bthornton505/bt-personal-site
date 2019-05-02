0393ded700a7de52145ab7eb9a686f45f9918bb7
Brenden Thornton <thorntonbrenden@gmail.com>
2019-05-02 15:43:34 -0600

    removed some code that wasn't being used

--- bt-site/src/components/Landing.js
+++ bt-site/src/components/Landing.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Mountains from '../images/mountains_v1.PNG'
+import Mountains from '../images/mountains_v7.PNG'
 
 const Landing = () => {
 
@@ -8,12 +8,11 @@ const Landing = () => {
       <div id="landing-picture">
         <img className="landing-image" src={Mountains} alt="mountains"></img>
       </div>
+      <div id="main-header">
+        <p className="job-title">Full Stack Engineer</p>
+      </div>
     </section>
   )
 }
 
 export default Landing
-
-// <div id="main-header">
-//   <p className="job-title">Full Stack Engineer</p>
-// </div>
