242d7a453e1e6bf4533f794336ff547ef7195c4f
Brenden Thornton <thorntonbrenden@gmail.com>
2019-05-03 11:18:49 -0600

    restructured some of the html and added new css classes

--- bt-site/src/components/Projects.js
+++ bt-site/src/components/Projects.js
@@ -9,31 +9,79 @@ const Projects = () => {
 
   return(
     <section id="projects">
+      <div className="project-headers">
+        <h2 className="project-title">Projects</h2>
+        <h3 className="project-caption">Check out some of my work!</h3>
+      </div>
+
       <Container id="projects-container">
-        <h2>Projects</h2>
-        <h3>Check out some of my work!</h3>
 
         <Row className="justify-content-center">
-        <Card className="project-slide" style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={Plantify} />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the bulk of
-              the card's content.
-            </Card.Text>
-          </Card.Body>
-          <ListGroup className="list-group-flush">
-            <ListGroupItem>Cras justo odio</ListGroupItem>
-            <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-            <ListGroupItem>Vestibulum at eros</ListGroupItem>
-          </ListGroup>
-          <Card.Body>
-            <Card.Link href="#">Card Link</Card.Link>
-            <Card.Link href="#">Another Link</Card.Link>
-          </Card.Body>
-        </Card>
+          <Card className="project-slide" style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={Plantify} />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroupItem>Cras justo odio</ListGroupItem>
+              <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+              <ListGroupItem>Vestibulum at eros</ListGroupItem>
+            </ListGroup>
+            <Card.Body>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
+            </Card.Body>
+          </Card>
         </Row>
+
+        <Row className="justify-content-center">
+          <Card className="project-slide" style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={Plantify} />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroupItem>Cras justo odio</ListGroupItem>
+              <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+              <ListGroupItem>Vestibulum at eros</ListGroupItem>
+            </ListGroup>
+            <Card.Body>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
+            </Card.Body>
+          </Card>
+        </Row>
+
+        <Row className="justify-content-center">
+          <Card className="project-slide" style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={Plantify} />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroupItem>Cras justo odio</ListGroupItem>
+              <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+              <ListGroupItem>Vestibulum at eros</ListGroupItem>
+            </ListGroup>
+            <Card.Body>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
+            </Card.Body>
+          </Card>
+        </Row>
+
       </Container>
     </section>
   )
