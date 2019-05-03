3173a2a113a4a0c32e0f523982f9a7125cd6863e
Brenden Thornton <thorntonbrenden@gmail.com>
2019-05-02 20:15:30 -0600

    changed structure of project card and added a new class for it

--- bt-site/src/components/Projects.js
+++ bt-site/src/components/Projects.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
+import { ListGroup, ListGroupItem }  from 'react-bootstrap'
+import Plantify from '../images/plantify_v2.png'
 
 const Projects = () => {
 
@@ -12,17 +14,25 @@ const Projects = () => {
         <h3>Check out some of my work!</h3>
 
         <Row className="justify-content-center">
-          <Card style={{ width: '16rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Plantify</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the bulk of
-                  the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
+        <Card className="project-slide" style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={Plantify} />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroupItem>Cras justo odio</ListGroupItem>
+            <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+            <ListGroupItem>Vestibulum at eros</ListGroupItem>
+          </ListGroup>
+          <Card.Body>
+            <Card.Link href="#">Card Link</Card.Link>
+            <Card.Link href="#">Another Link</Card.Link>
+          </Card.Body>
+        </Card>
         </Row>
       </Container>
     </section>
