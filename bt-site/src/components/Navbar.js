abe062a1c1f128a9f08a63641110c1e135e2f8e2
Brenden Thornton <thorntonbrenden@gmail.com>
2019-05-07 10:56:48 -0600

    created new navbar that uses a dropdown to show resume and blog links

--- bt-site/src/components/Navbar.js
+++ bt-site/src/components/Navbar.js
@@ -5,24 +5,45 @@ const Header = () => {
 
   return(
     <section id="navbar">
-      <ul id="bt-navbar">
-        <li className="navbar-nav">
-          <a className="navbar-links" href="/about">About</a>
-        </li>
-        <li className="navbar-nav">
-          <a className="navbar-links" href="/projects">Projects</a>
-        </li>
-        <li className="navbar-nav">
-          <a className="navbar-links" href="https://documentcloud.adobe.com/link/track?uri=urn%3Aaaid%3Ascds%3AUS%3Aedc78e2c-0fac-4dd8-94a4-706b2b6ad54f">
-            Resume
-          </a>
-        </li>
-        <li className="navbar-nav">
-          <a className="navbar-links" href="/contact">Contact</a>
-        </li>
-      </ul>
+      <div class="pos-f-t">
+        <div class="collapse" id="navbarToggleExternalContent">
+          <div class="bg-white p-4">
+            <a className="navbar-links" href="https://documentcloud.adobe.com/link/track?uri=urn%3Aaaid%3Ascds%3AUS%3Aedc78e2c-0fac-4dd8-94a4-706b2b6ad54f">
+              Resume
+            </a>
+            <a className="navbar-links" href="http://brendenthornton.com/">
+              Blog
+            </a>
+          </div>
+        </div>
+        <nav class="navbar navbar-light bg-white">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </nav>
+      </div>
     </section>
   )
 }
 
 export default Header;
+
+// <ul id="bt-navbar">
+//   <li className="navbar-nav">
+//     <a className="navbar-links" href="/about">About</a>
+//   </li>
+//   <li className="navbar-nav">
+//     <a className="navbar-links" href="/projects">Projects</a>
+//   </li>
+//   <li className="navbar-nav">
+    // <a className="navbar-links" href="https://documentcloud.adobe.com/link/track?uri=urn%3Aaaid%3Ascds%3AUS%3Aedc78e2c-0fac-4dd8-94a4-706b2b6ad54f">
+    //   Resume
+    // </a>
+//   </li>
+//   <li className="navbar-nav">
+//     <a className="navbar-links" href="/contact">Contact</a>
+//   </li>
+// </ul>
+
+// <nav class="nav flex-column">
+// </nav>
