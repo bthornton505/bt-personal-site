ea898de7edce685e0bb4012eebcfb37c159c343a
Brenden Thornton <thorntonbrenden@gmail.com>
2019-05-08 16:10:13 -0600

    updated the written content for the about

--- bt-site/src/components/About.js
+++ bt-site/src/components/About.js
@@ -13,24 +13,29 @@ const About = () => {
               Proud Father, Loving Husband, Ambitious Full-Stack Engineer.
             </h3>
             <p>
-              After working at the NBA for two years as a Phantom Camera Technician/Production Assistant,
-              I found myself at a crossroads. I no longer felt I was growing as a young professional
-              and realized I no longer enjoyed the career I had studied in college.
+              In January of 2018 I found myself at a crossroads. After working at
+              the NBA for two years as a Phantom Camera Technician/Production Assistant,
+              I no longer felt I was growing as a young professional. 
             </p>
             <p>
-              This pushed me to explore new opportunities which led me to discover the
-              Flatiron School, a programming bootcamp, and enroll in an online full-stack developer course.
-              I had no experience with code but always had a love for solving problems, and coding was just what I needed.
+              This pushed me to explore new opportunities which led me to discover
+              the Flatiron School, a programming bootcamp, and enroll in an online
+              Full Stack Web Developer course. I had no experience with code but always
+              had a love for solving problems, and knew I needed a career that would
+              challenge me forever.
             </p>
             <p>
-              Fast-forward 10 months from when I started and I have officially graduated
-              from this course with experience in building apps with Ruby on Rails, React, and a list of other technologies.
+              After ten months of hard work to learn the necessary skills to start
+              my coding career, my goal became a reality. Flatiron School gave me the
+              opportunity to build applications using Ruby on Rails, React, Redux,
+              and a list of other modern technologies.
             </p>
             <p>
-              The skills I have learned throughout this process have truly been life
-              changing and I know that whatever challenges I am faced with can be overcome.
-              This mindset is something I will bring with me everyday into the workplace,
-              and given the chance to join your team, I will prove that my work ethic will be my strongest quality.
+              As a goal-oriented person, I thrive on finding new and exciting challenges.
+              My current objective is to take the skills I have learned and to join a
+              tech company that will allow me to expand my current knowledge.
+              I hope I can bring my own unique perspective and personality to a team
+              that allows me to contribute meaningful solutions.
             </p>
           </Col>
         </Row>
